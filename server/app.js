d035903b05aa1c6c599833e0b51018d182dbc7e6
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2019-12-24 18:06:52 +0300

    added graphiql

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ const schema = require('./schema/schema');
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
 }));
 
 app.listen(4000, () => {
     console.info('Now listening for requests on port 4000');
 });
-
