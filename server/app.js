b21f9008a60cd8f8a2fcf3acd8be68fcbe071645
Siarhei Prystauka <siarhei_prystauka2@epam.com>
2019-12-26 18:34:40 +0300

    Added models, schemas and Mutation

--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+mongoose.connect(
+  'mongodb://test:test123@ds121336.mlab.com:21336/react-graphql'
+);
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
 app.listen(4000, () => {
-    console.info('Now listening for requests on port 4000');
+  console.info('Now listening for requests on port 4000');
 });
